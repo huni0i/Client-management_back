4972b680a1705cf1f1fe0fb21148e8eab3ff5d82
huni0i <huni0i@naver.com>
2026-01-07 23:13:35 +0900

    feat: 설정 클래스 추가 (Security, JWT, Swagger)

---
+++ src/main/java/com/counseling/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.counseling.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("Authorization")))
+                .info(new Info()
+                        .title("내담자 관리 서비스 API")
+                        .description("내담자 관리 서비스 API 명세서")
+                        .version("1.0.0"));
+    }
+}
+
